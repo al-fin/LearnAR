f33687f6e9ad6ca75f5c7d7eac5ad7d012676b89
Alfin <m.alfin.syahruddin@gmail.com>
2022-06-19 18:31:48 +0700

    Initial Commit

---
+++ LearnAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LearnAR
+//
+//  Created by Alfin on 19/06/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
